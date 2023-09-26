ab7a60f999274093343a16130989e108fac83160
yun <indl1670@gmail.com>
2023-09-26 15:05:39 +0900

    💜 [FEAT] 마이페이지 - 기본 컴포넌트 설정

---
+++ src/components/mypage/MyCredit.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function MyCredit() {
+  return (
+    <div>
+      <p className="text-2xl pb-5">크레딧 관리</p>
+    </div>
+  );
+}
--- src/pages/MyPage.tsx
+++ src/pages/MyPage.tsx
@@ -1,5 +1,35 @@
 import React from "react";
+import MyProfile from "../components/mypage/MyProfile";
+import AttendanceButton from "../components/mypage/AttendanceButton";
+import AttendanceCalendar from "../components/mypage/AttendanceCalendar";
+import MyCredit from "../components/mypage/MyCredit";
+import MyBoard from "../components/mypage/MyBoard";
+import MyScoreGraph from "../components/mypage/MyScoreGraph";
+import { authorityState } from "../recoil/atoms/common";
+import { useRecoilValue } from "recoil";
 
 export default function MyPage() {
-  return <div>마이페이지</div>;
+  const authority = useRecoilValue<string>(authorityState);
+  return (
+    <div className="mt-10 flex flex-col gap-10">
+      <p className="font-extrabold text-4xl">마이페이지</p>
+      <MyProfile />
+      {authority === "TRAINEE" ? (
+        <>
+          <div className="border-b-2 border-grayscale2"></div>
+          <AttendanceButton />
+          <div className="border-b-2 border-grayscale2"></div>
+          <AttendanceCalendar />
+          <div className="border-b-2 border-grayscale2"></div>
+          <MyScoreGraph />
+        </>
+      ) : (
+        ""
+      )}
+      <div className="border-b-2 border-grayscale2"></div>
+      <MyCredit />
+      <div className="border-b-2 border-grayscale2"></div>
+      <MyBoard />
+    </div>
+  );
 }
