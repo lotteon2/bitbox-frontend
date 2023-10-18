ff77dd89543d040e72015703526a177051798257
MJun111 <kmj0568@gmail.com>
2023-10-18 21:30:54 +0900

    ❤[FEAT] add board list css

--- src/pages/CommunityPage.tsx
+++ src/pages/CommunityPage.tsx
@@ -1,9 +1,9 @@
-import React, { useState } from "react";
+import React, {useState} from "react";
 
 export default function CommunityPage() {
   const [selectedCategory, setSelectedCategory] = useState<number>(0);
   const subCategory = ["✨   전체", "🤔   질문있어요", "🫂   공유해요"];
-  const search : string = ""
+  const search: string = ""
 
   return (
     <div className="flex flex-row my-10">
@@ -29,38 +29,78 @@ export default function CommunityPage() {
         </div>
       </div>
       <div className="w-full px-5 ml-20 mt-20">
-          <div
-              className="w-full h-9 left-36 top-9 bg-gray-300 rounded-xl mb-10"
-          >
-              <input
-                  className="w-full h-full rounded-xl bg-transparent outline-none px-2"
-                  type="text"
-                  placeholder="🔍 검색어를 입력하세요"
-              />
-          </div>
-      {selectedCategory === 0
+        <div
+          className="w-full h-9 left-36 top-9 bg-gray-300 rounded-xl mb-10"
+        >
+          <input
+            className="w-full h-full rounded-xl bg-transparent outline-none px-2"
+            type="text"
+            placeholder="🔍 검색어를 입력하세요"
+          />
+        </div>
+        {selectedCategory === 0
           ? (
+            <>
               <div>
-                  <div>카테고리1</div>
-                  <div>카테고리2</div>
-                  <div>카테고리n개 동적 생성</div>
-              </div>
-          )
-        : selectedCategory === 1
-        ? (
-            <>
-                <div>
-                    <div>질문있어요</div>
+                <div className="h-0.5 my-5 bg-gray-300"></div>
+                <div
+                  className="bg-sky-700 w-32 my-4 py-1 px-4 rounded-md text-center text-xl text-white font-thin">질문있어요
                 </div>
-                <div>공유해요</div>
+                <div className="flex">
+                  <div className="border-2 p-2 w-1/4 mx-2">
+                    <div className="text-lg font-medium m-2">게시글 제목</div>
+                    <div className="text-lg font-light mx-2">게시글 내용</div>
+                  </div>
+                  <div className="border-2 p-2 w-1/4 mx-2">
+                    <div className="text-lg font-medium m-2">게시글 제목</div>
+                    <div className="text-lg font-light mx-2">게시글 내용</div>
+                  </div>
+                  <div className="border-2 p-2 w-1/4 mx-2">
+                    <div className="text-lg font-medium m-2">게시글 제목</div>
+                    <div className="text-lg font-light mx-2">게시글 내용</div>
+                  </div>
+                  <div className="border-2 p-2 w-1/4 mx-2">
+                    <div className="text-lg font-medium m-2">게시글 제목</div>
+                    <div className="text-lg font-light mx-2">게시글 내용</div>
+                  </div>
+                </div>
+              </div>
+              <div>
+                <div className="h-0.5 my-5 bg-gray-300"></div>
+                <div
+                  className="bg-sky-900 w-32 my-4 py-1 px-4 rounded-md text-center text-xl text-white font-thin">공유해요
+                </div>
+                <div className="text-lg font-medium m-2">게시글 제목</div>
+                <div className="text-lg font-light mx-2">게시글 내용</div>
+              </div>
             </>
-              )
-        : (
-            <>
-
-            </>
-              )
-      }
+          )
+          : selectedCategory === 1
+            ? (
+              <>
+                <div>
+                  <div className="h-0.5 my-5 bg-gray-300"></div>
+                  <div
+                    className="bg-sky-700 w-32 my-4 py-1 px-4 rounded-md text-center text-xl text-white font-thin">질문있어요
+                  </div>
+                  <div className="text-lg font-medium m-2">게시글 제목</div>
+                  <div className="text-lg font-light mx-2">게시글 내용</div>
+                </div>
+              </>
+            )
+            : (
+              <>
+                <div>
+                  <div className="h-0.5 my-5 bg-gray-300"></div>
+                  <div
+                    className="bg-sky-900 w-32 my-4 py-1 px-4 rounded-md text-center text-xl text-white font-thin">공유해요
+                  </div>
+                  <div className="text-lg font-medium m-2">게시글 제목</div>
+                  <div className="text-lg font-light mx-2">게시글 내용</div>
+                </div>
+              </>
+            )
+        }
       </div>
     </div>
   );
