6ebbef8a96feb3069f2e9a5992023e33520d8457
indl1670 <81145399+indl1670@users.noreply.github.com>
2023-10-26 11:56:11 +0900

    💜[FIX] 이미지 업로드 경로 수정

--- src/apis/common/common.ts
+++ src/apis/common/common.ts
@@ -1,6 +1,6 @@
 import { authInstance } from "../utils";
 
 export const imageUpload = async (image: any) => {
-  const { data } = await authInstance.patch("/file-service/file/image", image);
+  const { data } = await authInstance.post("/file-service/file/image", image);
   return data;
 };
