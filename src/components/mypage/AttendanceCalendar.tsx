a986ef4b9d22ff3568eba3c06076641bd04c078c
yun <indl1670@gmail.com>
2023-11-01 17:21:33 +0900

    💜[FIX] console 삭제

--- src/components/mypage/AttendanceCalendar.tsx
+++ src/components/mypage/AttendanceCalendar.tsx
@@ -15,6 +15,7 @@ import { Button, Modal } from "antd";
 import { darkmodeState } from "../../recoil/atoms/common";
 import { Toast } from "../common/Toast";
 import { imageUpload } from "../../apis/common/common";
+import Loading from "../../pages/Loading";
 
 interface calendarEvents {
   id: number;
@@ -200,7 +201,8 @@ export default function AttendanceCalendar() {
       },
     }
   );
-  if (isLoading || data === undefined || events === undefined) return null;
+  if (isLoading || data === undefined || events === undefined)
+    return <Loading />;
 
   return (
     <>
